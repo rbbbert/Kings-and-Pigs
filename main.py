352cf4815f55c492320eea524d5296d869a5538e
rbbbert <robertbalan930@gmail.com>
2022-12-09 21:51:06 +0200

    Basic 2d movement implementation

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
-from sys import exit
+import sys
+from settings import screen_width
+from settings import screen_height
+from settings import level_map
+from level import Level
 
+# Pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1200, 700))
-pygame.display.set_caption('Runner')
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	pygame.display.update()
-	clock.tick(60)
+    screen.fill('black')
+    level.run()
+
+    pygame.display.update()
+    clock.tick(60)
